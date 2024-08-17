941bab66ace99bd613d91f2401bea3cb79fb7232
HashirAKB <ahmedhashir96@gmail.com>
2024-08-17 13:41:00 +0530

    Added: context text.

--- Garage/Solidity 101/Section 3 of Solidity 101 - FundMe/PriceConverter.sol
+++ Garage/Solidity 101/Section 3 of Solidity 101 - FundMe/PriceConverter.sol
@@ -1,6 +1,23 @@
 //SPDX-License-Identifier: MIT;
 pragma solidity ^0.8.18;
 
+/**
+Chainlink Data Feeds:
+Chainlink Data Feeds are the quickest way to connect your smart contracts to the real-world data such as asset prices, reserve balances, and L2 sequencer health.
+
+Data feeds provide many different types of data for your applications:
+Price Feeds
+Proof of Reserve Feeds
+Rate and Volatility Feeds
+L2 sequencer uptime feeds
+
+Price Feeds
+Chainlink Data Feeds provide data that is aggregated from many data sources by a decentralized set of independent node operators.
+
+To consume price data, your smart contract should reference AggregatorV3Interface, which defines the external functions implemented by Data Feeds.
+*/
+
+
 //import chainlink interface. This gives a minimalist ABI to interract with the contract.
 import {AggregatorV3Interface} from "@chainlink/contracts/src/v0.8/shared/interfaces/AggregatorV3Interface.sol";
 
